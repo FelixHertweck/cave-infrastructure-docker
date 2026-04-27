76ec4aa1a0b9f35fe06eb040b26232f5b8511eec
FelixHertweck <github@felix-hertweck.de>
2026-04-27 16:06:11 -0400

    fix: remove public flag from image upload command

--- scripts/upload-windows-image.sh
+++ scripts/upload-windows-image.sh
@@ -225,7 +225,6 @@ upload_image() {
         --file "$IMAGE_PATH" \
         --disk-format qcow2 \
         --container-format bare \
-        --public \
         "$IMAGE_NAME"
     
     # Success message
